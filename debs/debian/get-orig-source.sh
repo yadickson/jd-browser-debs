fb90df0a70e07e5c4cd25033e727a595b36d8e4c
yadickson <yadickson@gmail.com>
2015-06-16 18:11:59 -0430

    Add initial support to build jdownloader browser
    - Update readme file
    - Add debian folder to build this project

---
+++ debs/debian/get-orig-source.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+PKG="${1}"
+VERSION="${2}"
+ZIPFILE="${PKG}-${VERSION}.zip"
+ORIG_TARBALL="../${PKG}_${VERSION}.orig.tar.xz"
+
+[ ! -f "${ORIG_TARBALL}" ] || exit 0
+
+SHA=$(echo "${VERSION}" | awk -F'~' '{print $2}')
+VER=$(echo "${VERSION}" | awk -F'+' '{print $1}')
+
+rm -rf "${PKG}"*
+rm -rf "${PKG}-${VER}"
+rm -f "${ZIPFILE}"
+
+wget -c -t 1 -T 5 "https://github.com/svn2github/${PKG}/archive/${SHA}.zip" -O "${ZIPFILE}" || exit 1
+
+unzip "${ZIPFILE}" || exit 1
+
+rm -f "${ZIPFILE}"
+
+mv "${PKG}"* "${PKG}-${VER}"
+
+rm -rf "${PKG}-${VER}"/.settings
+rm -f "${PKG}-${VER}"/.classpath
+rm -f "${PKG}-${VER}"/.project
+rm -rf "${PKG}-${VER}"/test
+rm -f "${PKG}-${VER}"/*.iml
+rm -f "${PKG}-${VER}"/.git*
+
+tar -cJf "${ORIG_TARBALL}" --exclude-vcs "${PKG}-${VER}" || exit 1
+
+rm -rf "${PKG}-${VER}"
+rm -f "${ZIPFILE}"
+
